0ed408ec689801ff11a8aabf275bfeb470398d3e
kobbyeduah <kobbyeduah@gmail.com>
2024-03-31 22:08:55 +0000

    faker

---
+++ companies_data/Ghandour_cosmetics_DataPipeline_Faker/Ghandour_cosmetics_Faker.py
@@ -0,0 +1,33 @@
+from faker import Faker
+import pandas as pd
+import random
+ 
+fake = Faker('tw_GH')
+ 
+# Generate 100,000 records
+records = []
+for _ in range(100000):
+    individual = {
+        'customer_name': fake.name(),
+        'address': fake.address(),
+        'email': fake.email(),
+        'telephone': fake.phone_number(),
+        'country': 'Ghana',
+        'contact_preference': random.choice(['SMS', 'Email', 'Call']),
+
+        # Example transaction activity
+        'transaction_activity': fake.random_int(min=0, max=100),
+        'customer_preference': random.choice(['App', 'Website']),
+
+        # Default communication method
+        'communication_method': random.choice(['SMS', 'Email', 'Call'])
+    }
+ 
+    records.append(individual)
+ 
+data = pd.DataFrame(records)
+print(len(data))
+data.head()
+
+# save the data
+Ghandour_cosmetics_fakedata = data.to_csv('Ghandour_cosmetics.csv', index = False)
